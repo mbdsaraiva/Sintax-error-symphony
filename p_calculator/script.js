5494c91ef654e8ae8c19d937ef36360d2e9376cc
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-13 15:38:37 -0300

    xIsOfY, increase and discount implementation

--- p_calculator/script.js
+++ p_calculator/script.js
@@ -1,12 +1,39 @@
-function calcular(){
-    let value = document.getElementById("value").value;
-    let percentage = document.getElementById("percentage").value;
+function calcular() {
+    let value = parseFloat(document.getElementById("value").value);
+    let percentage = parseFloat(document.getElementById("percentage").value);
+    let type = document.getElementById("calcType").value;
 
-    if (value == "" || percentage == ""){
-        alert("Please, fill out both fields!")
+    if (isNaN(value) || isNaN(percentage)) {
+        alert("Please fill in both fields!");
         return
     }
 
-    let result = (value * percentage) / 100;
-    document.getElementById("result").innerText = `Result: ${result}%`;
+    let result;
+
+    switch (type) {
+
+        case "xOfY":
+            result = (value * percentage) / 100;
+            document.getElementById("result").innerText = `${percentage}% of ${value} is: ${result}`;
+            break;
+
+        case "xIsOfY":
+            result = (value / percentage) * 100;
+            document.getElementById("result").innerText = `${value} is ${result.toFixed(2)}% of ${percentage}`;
+            break;
+
+
+        case "increase":
+            result = value + (value * percentage) / 100;
+            document.getElementById("result").innerText = `${value} increased by ${percentage}% is: ${result}`;
+            break;
+
+        case "discount":
+            result = value-(value * percentage) / 100
+            document.getElementById("result").innerText = `${value} decreased by ${percentage}% is: ${result}`;
+            break;
+
+        default:
+            alert("Please select a calculation type!")
+    }
 }
